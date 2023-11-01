9f806f2741ed9594fcdd09dc5af1e986931a02a8
TeddySawon <84612523+TeddySawon@users.noreply.github.com>
2023-11-01 14:43:40 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Contoh kode JavaScript untuk interaksi (opsional)
+
+// Fungsi untuk menampilkan pesan saat halaman dimuat
+window.onload = function() {
+    alert("Selamat datang di portofolio saya!");
+};
