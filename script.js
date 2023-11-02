eacb8dbbcb18783e481d140f7473397807210884
TeddySawon <84612523+TeddySawon@users.noreply.github.com>
2023-11-02 19:46:38 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Contoh kode JavaScript untuk interaksi (opsional)
-
-// Fungsi untuk menampilkan pesan saat halaman dimuat
-window.onload = function() {
-    alert("Selamat datang di portofolio saya!");
-};
